2a057a7d0d042694be5581fa255872d9c5617f7a
SigismundM30 <2096201936@qq.com>
2023-10-25 16:31:12 +0800

    1.0--1.1

--- ThingTeamProject/src/Driver.java
+++ ThingTeamProject/src/Driver.java
@@ -1,6 +1,6 @@
 public class Driver {
     public static void main(String[] args) {
 
-        System.out.println("Thing Team Project V1.0");
+        System.out.println("Thing Team Project V1.1");
     }
 }
